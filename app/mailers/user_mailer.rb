262db114830619f987365809f1b183fb36485f96
pwz2k <pwz2k@hotmail.com>
2015-07-06 22:04:58 -0400

    email titles

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,16 +9,16 @@ class UserMailer < ActionMailer::Base
   
   def registration_confirmation(user)
     @user = user
-    mail(:to => user.email, :subject => "Registered")
+    mail(:to => user.email, :subject => "Welcome to AreYouTaken — the Dating Site That Goes Offline‏")
   end
   
   def password_reset(user)
     @user = user
-    mail :to => user.email, :subject => "Password Reset"
+    mail :to => user.email, :subject => "Reset Your Password For AreYouTaken"
   end
   
   def subscriber(user)
     @user = user
-    mail(:to => user.email, :subject => "Congrats! Here's Your Subscription Information")
+    mail(:to => user.email, :subject => "Your Subscription Information For AreYouTaken")
   end
 end
